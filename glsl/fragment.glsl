23a355dff550238d33c8a5ab657367d12ec6d2e0
rcrimp <reubencrimp@gmail.com>
2015-11-11 21:18:28 +1300

    fancy shaders

--- glsl/fragment.glsl
+++ glsl/fragment.glsl
@@ -1,7 +1,8 @@
 #version 150 core
-
 out vec4 outColour;
-
+in vec3 normal;
+in vec3 light;
 void main() {
-   outColour = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+   float i = max(0.1, dot(normal, light));
+   outColour = vec4(i, i, i, 1.0f);
 }
--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -1,11 +1,13 @@
 #version 150 core
-
 in vec3 position;
-
+out vec3 normal;
+out vec3 light;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
-
 void main() {
+   normal = normalize(position);
+   vec4 t = inverse(model) * vec4(10, 1, 5, 1);
+   light = normalize(vec3(t.x, t.y, t.z));
   gl_Position = proj * view * model * vec4(position, 1.0);
 }
